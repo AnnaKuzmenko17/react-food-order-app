e32f4cf5b8cfb59e8e9c1728c90f810cb6fc1962
ANNA KUZMENKO <anna.kuzmenko120063@gmail.com>
2024-04-17 23:22:12 +0300

    styling fixed, added actions on meal cards

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -5,6 +5,10 @@ import { currFormatter } from "../util/formatting";
 
 export default function MealItem({ meal }) {
   const cartCtx = useContext(CartContext);
+  console.log(meal, cartCtx.items);
+  const existingIndex = cartCtx.items.findIndex((item) => item.id === meal.id);
+  const isAddedToCart = existingIndex !== -1;
+  const quantity = isAddedToCart ? cartCtx.items[existingIndex].quantity : 0;
 
   function handleAddMealToCart() {
     cartCtx.addItem(meal);
@@ -19,8 +23,16 @@ export default function MealItem({ meal }) {
           <p className="meal-item-price">{currFormatter.format(meal.price)}</p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
-        <p className="meal-item-actions">
-          <Button onClick={handleAddMealToCart}>Add to cart</Button>
+        <p>
+          {isAddedToCart ?
+            <p className="meal-item-actions">
+              <button onClick={() => cartCtx.removeItem(meal.id)}>-</button>
+              <span>{quantity}</span>
+              <button onClick={() => cartCtx.addItem(meal)}>+</button>
+            </p>
+            :
+            <Button onClick={handleAddMealToCart}>Add to cart</Button>
+          }
         </p>
       </article>
     </li>
